c2add8720075638eb2eb42b5dbf92d0646f476be
SamatarCodes <thewind07@protonmail.com>
2020-05-11 16:41:47 -0400

    added kActiveColor TextStyle

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'main.dart';
 
 const kButtonColor = Color(0XFF4E535F);
 const kTextStyle = TextStyle(
   fontSize: 20.0,
   fontWeight: FontWeight.w300,
 );
+
+const kTestingColor = TextStyle(
+  fontSize: 20.0,
+);
+
+const kActiveColor = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w600,
+  fontSize: 20.0,
+);
+
+//const kActiveColor = TextStyle(Theme.of(context).textTheme.headline);
